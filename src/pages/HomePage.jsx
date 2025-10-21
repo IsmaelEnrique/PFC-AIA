9254c800c08953d68852dc89d3cb45eb78befbeb
prichanga <priscilarouiller@gmail.com>
2025-10-21 09:54:30 -0300

    Agregué botón en HomePage

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,8 +1,14 @@
+
 export default function HomePage() {
   return (
     <div style={{ padding: '2rem' }}>
       <h1>Bienvenido a tu tienda personalizable 🛍️</h1>
       <p>Mostrá tus productos, gestioná tu stock y vendé en línea fácilmente.</p>
+
+      
+      <button style={{ marginTop: '1rem', padding: '0.5rem 1rem' }}>
+        Hola que tal soy pp
+      </button>
     </div>
   )
 }
