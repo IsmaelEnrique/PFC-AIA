1656a19585434789fc02c92112dd70b1e2698fb8
IsmaelEnrique <ismaeljulianenrique@gmail.com>
2026-01-28 17:08:22 -0300

    colgue con cargar lo del use state xd

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,5 @@
 import { Routes, Route, Navigate } from "react-router-dom";
+import { useState } from "react";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import Home from "./pages/HomePage";
